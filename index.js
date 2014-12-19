dd042fa456a8f324b850bad4019551e00c5ea2de
Adam Keesecker <adamkeesecker@gmail.com>
2014-12-18 18:57:29 -0600

    creates unique divs for each rule. Clean-up required

--- index.js
+++ index.js
@@ -8,18 +8,43 @@
   function init(){
 //------My listeners:--------
     $('#submitBaseRate').click(setBaseRate);
-    // $('#').click();
-    // $('#').click();
-    // $('#').click();
+    $('#addRule').click(newRule);
   };
 
+  var originBaseRate = 0;
+  var totalRules = 0;
+
   function display(newRate){
-    $('#currentRate').text(newRate);
-  }
+    $('#currentRate').text('Your current rate: '+newRate+'/hr');
+  };
 
   function setBaseRate(){
-    var originBaseRate = $('#baseRate').val();
+    originBaseRate = $('#baseRate').val();
     display(originBaseRate);
+    $('#baseRateContainer').hide();
+    $('#controlsContainer').css("visibility", "visible");
+    $('#disappear').fadeOut({duration:5200});
+  };
+
+  function newRule(){
+    totalRules += 1;
+
+    var bigString = "<div id=div"
+                    +totalRules.toString()+
+                    "><input id=input"
+                    +totalRules+
+                    "><button>X</button></div>"
+
+    $('#ruleDisplay').append(bigString);
+
+
+    // var divBegin =
+    // var divEnd =
+    // var ruleBegin = '<input id=rule';
+    // var ruleEnd = '><button>x</button><br>'
+    //
+    // var concatRule = 'ruleBegin+totalRules+ruleEnd
+    // $('#ruleDisplay').append(concatRule);
   };
 
   // function increaseRate(){};
