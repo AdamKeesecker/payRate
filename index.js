048285f984ffaf778e7ef5752aafe0060a1529e5
Adam Keesecker <adamkeesecker@gmail.com>
2014-12-19 18:50:23 -0600

    logic skeleton in place. Math time go

--- index.js
+++ index.js
@@ -3,13 +3,14 @@
 
 
   $(document).ready(init);
-//------on page-load, prepare listeners-------
+  /*on page-load, prepare listeners*/
 
   function init(){
-//------My listeners:--------
+    /*My listeners:*/
     $('#submitBaseRate').on('click', setBaseRate);
     $('#addRule').on('click', newRule);
     $('#ruleDisplay').on('click', '.closeButton', this, deleteRow);
+    $('#calculate').on('click', calculate);
   };
 
   var originBaseRate = 0;
@@ -27,25 +28,60 @@
   };
 
   function newRule(){
-    /*
-      rowString creates a select, two inputs, and a close button
-
+    /*    rowString creates a select, two inputs, and a close button
       div
         select
-          option
-          option
+          option (+)
+          option (-)
         input (Base rate modifier)
         input (Modifier description)
         button (delete row)
-
     */
 
-    var rowString = '<div><select><option value="plus">+</option><option value="minus">-</option></select><input class="baseRateMod" placeholder="Base rate mod"/><input class="modDescription" placeholder="Description"/><button class="closeButton">X</button></div>';
+    var rowString = '<div class="row">$<input class="baseRateMod" placeholder="-10"/>/hr<br><input class="modDescription" placeholder="Description"/><button class="closeButton">X</button></div>';
     $('#ruleDisplay').append(rowString);
   };
 
   function deleteRow(row){
     $(row.currentTarget).parent().remove();
+  };
+
+  function calculate(){
+    /* CONDENSE THIS!!!!! */
+    var mod;
+    var $base = $('.baseRateMod');
+    if($base.length > 1){
+      mod = $base;
+      for(var i =0; i < $base.length; i++){
+        if(mod[i].value == ""){
+          console.log('null');
+        }else if(mod[i].value > 0){
+          console.log('positive number');
+        }else if(mod[i].value == 0){
+          console.log('zero');
+        }else if(mod[i].value < 0){
+          console.log('negative number');
+        }else{
+          console.log('error catch');
+        }
+      }
+    }else{
+      mod = $base.val();
+      for(var i =0; i < $base.length; i++){
+        console.log(mod);
+        if(mod == ""){
+          console.log('null');
+        }else if(mod > 0){
+          console.log('positive number');
+        }else if(mod == 0){
+          console.log('zero');
+        }else if(mod < 0){
+          console.log('negative number');
+        }else{
+          console.log('error catch');
+        };
+      }
+    }
   };
 
   // function increaseRate(){};
